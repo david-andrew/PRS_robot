f8cee9b701a3892c0c44c21a33a3e9994ce61fad
David A. Samson <david.andrew.engineer@gmail.com>
2019-05-02 23:37:18 -0400

    updated slide alignment

--- RobotDriver/Robot.h
+++ RobotDriver/Robot.h
@@ -23,7 +23,7 @@
 
 #define LASER_ALIGNMENT_OFFSET -52      //number of steps offset from slot positions to the laser axis location
 #define GLUE_ALIGNMENT_OFFSET 4436      //number of steps offset from slot positions to the glue needle location
-#define PRESS_ALIGNMENT_OFFSET 13487    //number of steps offset from slot positions to the press arm location
+#define PRESS_ALIGNMENT_OFFSET 13510    //number of steps offset from slot positions to the press arm location
 
 #define SLOT_GROUP_SIZE 5               //number of slots to press/glue at a time.
 // #define CLIP_LOCATION 12/13 14/15    //some way of locating the clip clamping the fretboard
